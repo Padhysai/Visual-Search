11c8941a46cb6bfd5c3bc2f3ec93ab022d2444ff
Sai Prasad Padhy <saiprasadpadhy@gmail.com>
2020-12-26 14:47:33 +0530

    config changes

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class DevelopmentConfig(Config):
     BASEPATH = os.path.dirname(__file__)
     IMAGE_UPLOAD_LOCATION = os.path.join(BASEPATH, 'data/input_image', 'input.jpg')
     MODEL_NAME = 'VGG'
-    NO_OF_SIMILAR_IMAGES = 4
+    NO_OF_SIMILAR_IMAGES = 6
     DATASET = 'my_dataset'
     #DATASET_IMAGES_PATH = os.path.join(BASEPATH, 'data/dataset', DATASET)
-    DATASET_IMAGES_PATH = os.path.join('app/static', 'images')
+    DATASET_IMAGES_PATH = os.path.join(BASEPATH, 'data','dataset',DATASET)
     MODEL_PATH = os.path.join(BASEPATH, 'data/models', 'model.h5')
